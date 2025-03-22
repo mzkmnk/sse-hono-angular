9853090c11e298cf95ec7b3af18cfee5bc9bd3ff
mzkmnk <sma0729mex@gmail.com>
2025-03-22 20:05:13 +0900

    refactor: migrate post to get query

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,14 +1,8 @@
-import {zValidator} from '@hono/zod-validator';
 import {Hono} from 'hono';
 import {env} from 'hono/adapter';
 import {cors} from 'hono/cors';
 import {streamSSE} from 'hono/streaming';
 import OpenAI from 'openai';
-import {z} from 'zod';
-
-const schema = z.object({
-	message: z.string(),
-});
 
 const app = new Hono()
 	.use(
@@ -17,50 +11,37 @@ const app = new Hono()
 			origin: ['http://localhost:4200'],
 		}),
 	)
-	.post(
-		'/stream/messages',
-		zValidator('json', schema, (result, c) => {
-			if (!result.success) {
-				return c.json(
-					{
-						message: 'Invalid request',
-					},
-					400,
-				);
-			}
-		}),
-		async (c) => {
-			const { OPENAI_API_KEY } = env<{ OPENAI_API_KEY: string }>(c);
+	.get('/stream/messages/:message', async (c) => {
+		const { OPENAI_API_KEY } = env<{ OPENAI_API_KEY: string }>(c);
 
-			const openai = new OpenAI({
-				apiKey: OPENAI_API_KEY,
-			});
+		const openai = new OpenAI({
+			apiKey: OPENAI_API_KEY,
+		});
 
-			const { message } = c.req.valid('json');
+		const message: string = c.req.param('message');
 
-			const streamChunk = await openai.chat.completions.create({
-				model: 'gpt-4o-mini',
-				messages: [
-					{
-						role: 'user',
-						content: message,
-					},
-				],
-				stream: true,
-			});
+		const streamChunk = await openai.chat.completions.create({
+			model: 'gpt-4o-mini',
+			messages: [
+				{
+					role: 'user',
+					content: message,
+				},
+			],
+			stream: true,
+		});
 
-			return streamSSE(c, async (stream) => {
-				for await (const chunk of streamChunk) {
-					if (chunk.choices.length === 0) {
-						return;
-					}
-
-					await stream.writeSSE({
-						data: chunk.choices[0].delta.content ?? '',
-					});
+		return streamSSE(c, async (stream) => {
+			for await (const chunk of streamChunk) {
+				if (chunk.choices.length === 0) {
+					return;
 				}
-			});
-		},
-	);
+
+				await stream.writeSSE({
+					data: chunk.choices[0].delta.content ?? '',
+				});
+			}
+		});
+	});
 
 export default app;
