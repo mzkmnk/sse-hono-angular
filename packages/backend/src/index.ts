6fa03dfe0fd090114a582be9271ec5a765dc5581
mzkmnk <sma0729mex@gmail.com>
2025-03-22 18:39:05 +0900

    feat: add message stream

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,67 @@
-import { Hono } from 'hono';
+import {zValidator} from '@hono/zod-validator';
+import {Hono} from 'hono';
+import {env} from 'hono/adapter';
+import {cors} from 'hono/cors';
+import {streamSSE} from 'hono/streaming';
+import OpenAI from 'openai';
+import {z} from 'zod';
 
-const app = new Hono();
-
-app.get('/', (c) => {
-	return c.text('Hello Hono!');
+const schema = z.object({
+	message: z.string(),
 });
+
+const app = new Hono()
+	.use(
+		'/*',
+		cors({
+			origin: ['http://localhost:4200'],
+		}),
+	)
+	.post(
+		'/stream/messages',
+		zValidator('json', schema, (result, c) => {
+			console.log(result);
+			if (!result.success) {
+				return c.json(
+					{
+						message: 'Invalid request',
+					},
+					400,
+				);
+			}
+		}),
+		async (c) => {
+			const { OPENAI_API_KEY } = env<{ OPENAI_API_KEY: string }>(c);
+
+			const openai = new OpenAI({
+				apiKey: OPENAI_API_KEY,
+			});
+
+			const { message } = c.req.valid('json');
+
+			const streamChunk = await openai.chat.completions.create({
+				model: 'gpt-4o-mini',
+				messages: [
+					{
+						role: 'user',
+						content: message,
+					},
+				],
+				stream: true,
+			});
+
+			return streamSSE(c, async (stream) => {
+				for await (const chunk of streamChunk) {
+					if (chunk.choices.length === 0) {
+						return;
+					}
+
+					await stream.writeSSE({
+						data: chunk.choices[0].delta.content ?? '',
+					});
+				}
+			});
+		},
+	);
 
 export default app;
