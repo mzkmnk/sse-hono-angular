8a78ed9fa7180329ab64a36e23da73b2a24e8fe2
mizuki <sma0729mex@gmail.com>
2025-03-22 22:49:20 +0900

    feat: streaming (#4)
    * feat: add tailwind
    
    * refactor: 不必要なファイルの削除
    
    * refacgtor: angular configの修正
    
    * chore: add .idea
    
    * feat: add message stream
    
    * fix: dependency
    
    * chore: angularの設定まわり
    
    * chore: logの削除
    
    * chore: add icons
    
    * feat: add message ui
    
    * refactor: migrate post to get query
    
    * refactor: readme etc..
    
    * refactor: run biome

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,47 @@
 import { Hono } from 'hono';
+import { env } from 'hono/adapter';
+import { cors } from 'hono/cors';
+import { streamSSE } from 'hono/streaming';
+import OpenAI from 'openai';
 
-const app = new Hono();
+const app = new Hono()
+	.use(
+		'/*',
+		cors({
+			origin: ['http://localhost:4200'],
+		}),
+	)
+	.get('/stream/messages/:message', async (c) => {
+		const { OPENAI_API_KEY } = env<{ OPENAI_API_KEY: string }>(c);
 
-app.get('/', (c) => {
-	return c.text('Hello Hono!');
-});
+		const openai = new OpenAI({
+			apiKey: OPENAI_API_KEY,
+		});
+
+		const message: string = c.req.param('message');
+
+		const streamChunk = await openai.chat.completions.create({
+			model: 'gpt-4o-mini',
+			messages: [
+				{
+					role: 'user',
+					content: message,
+				},
+			],
+			stream: true,
+		});
+
+		return streamSSE(c, async (stream) => {
+			for await (const chunk of streamChunk) {
+				if (chunk.choices.length === 0) {
+					return;
+				}
+
+				await stream.writeSSE({
+					data: chunk.choices[0].delta.content ?? '',
+				});
+			}
+		});
+	});
 
 export default app;
